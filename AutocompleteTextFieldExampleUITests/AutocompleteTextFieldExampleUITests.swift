85002f0354b3d89349b13e845f5679d5ee092de2
Brian Nicholson <thebnich@gmail.com>
2017-04-24 11:58:41 -0700

    Add some basic UI tests

---
+++ AutocompleteTextFieldExampleUITests/AutocompleteTextFieldExampleUITests.swift
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import XCTest
+
+class AutocompleteTextFieldExampleUITests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        let field = XCUIApplication().textFields["Domain"]
+        field.tap()
+
+        field.typeText("w")
+        XCTAssertEqual(field.value! as! String, "www.google.com/")
+        field.typeText(XCUIKeyboardKeyDelete)
+        XCTAssertEqual(field.value! as! String, "w")
+        field.typeText("ww.yah")
+        XCTAssertEqual(field.value! as! String, "www.yahoo.com/")
+        field.typeText(XCUIKeyboardKeyDelete)
+        XCTAssertEqual(field.value! as! String, "www.yah")
+        field.typeText(XCUIKeyboardKeyDelete)
+        XCTAssertEqual(field.value! as! String, "www.ya")
+        field.typeText(XCUIKeyboardKeyDelete)
+        field.typeText(XCUIKeyboardKeyDelete)
+        field.typeText(XCUIKeyboardKeyDelete)
+        field.typeText(XCUIKeyboardKeyDelete)
+        field.typeText(XCUIKeyboardKeyDelete)
+        field.typeText(XCUIKeyboardKeyDelete)
+        XCTAssertEqual(field.value! as! String, "")
+        field.typeText("g")
+        XCTAssertEqual(field.value! as! String, "google.com/")
+        field.typeText("z")
+        XCTAssertEqual(field.value! as! String, "gz")
+        field.typeText(XCUIKeyboardKeyDelete)
+        XCTAssertEqual(field.value! as! String, "g")
+        field.typeText("o")
+        XCTAssertEqual(field.value! as! String, "google.com/")
+    }
+}
